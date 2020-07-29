352aabf28f1ace5134450ea545d60fed80ad523c
Cristhian Daza <cristian.d.b@hotmail.com>
2020-07-29 17:12:33 -0500

    Eliminando Clientes en la base de datos

--- src/components/Clientes.jsx
+++ src/components/Clientes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import { CLIENTES_QUERY } from '../queries';
 import { ELIMINAR_CLIENTE } from '../mutations';
 
@@ -14,29 +14,38 @@ const Clientes = () => (
         <>
           <h2 className="text-center">Listado Clientes</h2>
           <ul className="list-group mt-4">
-            {data.getClientes.map(item => (
-              <li key={item.id} className="list-group-item">
-                <div className="row justify-content-between align-items-center">
-                  <div className="col-md-8 d-flex justify-content-between align-items-center">
-                    {item.nombre} {item.apellido}
+            {data.getClientes.map(item => {
+              const { id } = item
+              return (
+                <li key={item.id} className="list-group-item">
+                  <div className="row justify-content-between align-items-center">
+                    <div className="col-md-8 d-flex justify-content-between align-items-center">
+                      {item.nombre} {item.apellido}
+                    </div>
+                    <div className="col-md-4 d-flex justify-content-end">
+                      <Mutation mutation={ELIMINAR_CLIENTE}>
+                        {eliminarCliente => (
+                          <button 
+                          type="button"
+                          className="btn btn-danger db-block d-md-inline-block mr-2"
+                          onClick={() => {
+                            eliminarCliente({
+                              variables: { id }
+                            })
+                          }}
+                          >
+                            &times; Eliminar
+                          </button>
+                        )}
+                      </Mutation>
+                      <Link className="btn btn-success d-block d-md-inline-block" to={`/cliente/editar/${item.id}`}>
+                        Editar Cliente
+                      </Link>
+                    </div>
                   </div>
-                  <div className="col-md-4 d-flex justify-content-end">
-                    <button 
-                      type="button"
-                      className="btn btn-danger db-block d-md-inline-block mr-2"
-                      onClick={() => {
-                        console.log(item.id)
-                      }}
-                    >
-                      &times; Eliminar
-                    </button>
-                    <Link className="btn btn-success d-block d-md-inline-block" to={`/cliente/editar/${item.id}`}>
-                      Editar Cliente
-                    </Link>
-                  </div>
-                </div>
-              </li>
-            ))}
+                </li>
+              )
+            })}
           </ul>
         </>
       )
